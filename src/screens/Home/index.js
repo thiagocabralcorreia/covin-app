8a50ab4cd134413ebf7098fbdf98d7818df30e70
Thiago Cabral Correia <thiagocabralcorreia@gmail.com>
2022-01-16 15:09:14 -0300

    First commit

---
+++ src/screens/Home/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+function Home() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>CovIn - Covid-19 Information</Text>
+    </View>
+  );
+}
+
+export default Home;
