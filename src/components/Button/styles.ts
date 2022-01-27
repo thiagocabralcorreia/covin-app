1533bf2614c9a90c075764d524ea862c96a53532
Thiago Cabral Correia <thiagocabralcorreia@gmail.com>
2022-01-26 22:07:45 -0300

    Add Button components

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet, Dimensions } from 'react-native';
+import colors from '../../colors';
+
+const styles = StyleSheet.create({
+	button: {
+		flexDirection: 'row',
+        backgroundColor: colors.orange,
+        height: 56,
+		width: Dimensions.get('window').width * 0.53,
+        borderRadius: 16,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+		paddingHorizontal: 20,
+		marginBottom: 10,
+    },
+    buttonText: {
+        fontSize: 16,
+        color: colors.white,
+        fontFamily: 'Jost_600SemiBold',
+		marginLeft: 16,
+    }
+})
+
+export default styles;
