05eefe39bf9c1c2c00b6ccc241f93dbb1bd53b30
Thiago Cabral Correia <thiagocabralcorreia@gmail.com>
2022-01-27 21:25:48 -0300

    Add GlobalSituation screen, update routes and tools

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import { View, Text, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {RootStackParamList} from '../../routes';
 import styles from './styles';
 
 import logo from '../../../assets/covin_logo2.png';
@@ -7,32 +10,36 @@ import { Button } from '../../components/Button';
 
 import { FontAwesome, FontAwesome5, Foundation } from '@expo/vector-icons';
 
-function Home() {
-  return (
-	<View style={styles.container}>
-		<Image source={logo} style={styles.logo} />
-		<View style={styles.contentContainer}>
-			<Text style={styles.title}>
-				COVID-19 INFORMATION{'\n'}
-				Epidemiological Update
-			</Text>
-			<Text style={styles.text}>
-				Stay informed about Coronavirus cases around the world. Also check the situation in your country and essential prevention tips.
-			</Text>
-			<View style={styles.buttonContainer}>
-				<Button title='Global Level'>
-					<FontAwesome5 name='globe' size={24} color='white' />
-				</Button>
-				<Button title='Country Level'>
-					<FontAwesome name='flag' size={24} color='white' />
-				</Button>
-				<Button title='Prevention Tips'>
-					<Foundation name='info' size={30} color='white' />
-				</Button>
+type homeProp = StackNavigationProp<RootStackParamList, 'Home'>;
+
+const Home: React.FC = () => {
+  const navigation = useNavigation<homeProp>();
+
+	return (
+		<View style={styles.container}>
+			<Image source={logo} style={styles.logo} />
+			<View style={styles.contentContainer}>
+				<Text style={styles.title}>
+					COVID-19 INFORMATION{'\n'}
+					Epidemiological Update
+				</Text>
+				<Text style={styles.text}>
+					Stay informed about Coronavirus cases around the world. Also check the situation in your country and essential prevention tips.
+				</Text>
+				<View style={styles.buttonContainer}>
+					<Button title='Global Situation' onPress={() => navigation.navigate('GlobalSituation')}>
+						<FontAwesome5 name='globe' size={24} color='white'/>
+					</Button>
+					<Button title='Country Situation'>
+						<FontAwesome name='flag' size={24} color='white' />
+					</Button>
+					<Button title='Prevention Tips'>
+						<Foundation name='info' size={30} color='white' />
+					</Button>
+				</View>
 			</View>
 		</View>
-	</View>
-  );
+	);
 }
 
 export default Home;
